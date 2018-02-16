9ef1e563842707d9999bd22fede1071047c6d8b1
martincarina <martincarina@mail.ru>
2018-02-17 01:05:40 +0700

    Из getUnion  и getDifference удалены переменные копий объектов и в них же реализовано условие отсутствия пересечения без getIntersection.

--- Range/src/ru/academits/martin/range/Range.java
+++ Range/src/ru/academits/martin/range/Range.java
@@ -47,10 +47,8 @@ public class Range {
     }
 
     public Range getIntersection(Range interval2) {
-        double begin;
-        double end;
-        begin = Math.max(this.from, interval2.from);
-        end = Math.min(interval2.to, this.to);
+        double begin = Math.max(this.from, interval2.from);
+        double end = Math.min(interval2.to, this.to);
         if (end > begin) {
             return new Range(begin, end);
         } else {
@@ -59,25 +57,22 @@ public class Range {
     }
 
     public Range[] getUnion(Range interval2) {
-        Range interval1Copy = new Range(this);
-        Range interval2Copy = new Range(interval2);
-        if (this.getIntersection(interval2) == null) {
-            return new Range[]{interval1Copy, interval2Copy};
+        if (interval2.from >= this.to || this.from >= interval2.to) {
+            return new Range[]{new Range(this), new Range(interval2)};
         } else if (interval2.from >= this.from && interval2.to >= this.to) {
             return new Range[]{new Range(this.from, interval2.to)};
         } else if (interval2.from >= this.from) {
-            return new Range[]{interval1Copy};
+            return new Range[]{new Range(this)};
         } else if (this.from > interval2.from && this.to >= interval2.to) {
             return new Range[]{new Range(interval2.from, this.to)};
         } else {
-            return new Range[]{interval2Copy};
+            return new Range[]{new Range(interval2)};
         }
     }
 
     public Range[] getDifference(Range interval2) {
-        Range interval1Copy = new Range(this);
-        if (this.getIntersection(interval2) == null) {
-            return new Range[]{interval1Copy};
+        if (interval2.from >= this.to || this.from >= interval2.to) {
+            return new Range[]{new Range(this)};
         } else if (interval2.from > this.from && this.to > interval2.to) {
             return new Range[]{new Range(this.from, interval2.from), new Range(interval2.to, this.to)};
         } else if (interval2.from > this.from) {
