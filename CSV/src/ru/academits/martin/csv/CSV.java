9aabc07d03d126a7384384732dee09772cc351c6
martincarina <martincarina@mail.ru>
2018-02-22 01:03:27 +0700

    Добавлена функция handleSymbol, проверка количества аргументов программы, пробелы и табы рассматриваются, как обычные символы, основной код переписан для одной булевой переменной

--- CSV/src/ru/academits/martin/csv/CSV.java
+++ CSV/src/ru/academits/martin/csv/CSV.java
@@ -6,124 +6,127 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class CSV {
-    public static void main(String[] args) throws FileNotFoundException {
+    private static String handleSymbol(char symbol) {
+        if (symbol == '>') {
+            return "&gt;";
+        } else if (symbol == '<') {
+            return "&lt;";
+        } else if (symbol == '&') {
+            return "&amp;";
+        } else {
+            return Character.toString(symbol);
+        }
+    }
 
+    public static void main(String[] args) throws FileNotFoundException {
+        if (args.length != 2) {
+            System.out.println("Передано неверное количество аргументов.");
+            System.out.println("Нужно передать 2 аргумента: название входного файла в формате *.csv и название выходного файла в формате *html.");
+            return;
+        }
         try (PrintWriter writer = new PrintWriter(args[1]);
              Scanner scanner = new Scanner(new FileInputStream(args[0]))) {
-            boolean isInsideSimpleCell = false;
             boolean isInsideComplexCell = false;
-            boolean isComplexCellClosed = false;
 
             writer.println("<!doctype html>");
             writer.println("<html>");
             writer.println("<head>");
             writer.println("<meta charset=\"utf-8\">");
-            writer.println("<title>" + "Таблица" + "</title>");
+            writer.println("<title>Таблица</title>");
             writer.println("</head>");
             writer.println("<body>");
             writer.println("<table>");
+
             while (scanner.hasNext()) {
                 String line = scanner.nextLine();
                 int index = 0;
                 final char QUOTE = '\"';
                 final char COMMA = ',';
-                final char SPACE = ' ';
-                final char TAB = '\t';
-                final char GT = '>';
-                final char LT = '<';
-                final char AMP = '&';
-
-                if (!isInsideComplexCell) {
-                    writer.print("<tr>");
-                }
 
                 while (index < line.length()) {
-                    char c = line.charAt(index);
-                    switch (c) {
-                        case QUOTE:
-                            if (isInsideComplexCell) {
-                                if (index == line.length() - 1) {
-                                    writer.print("</td>");
+                    char symbol = line.charAt(index);
+                    if (isInsideComplexCell) {
+                        if (index == line.length() - 1) {
+                            if (symbol == QUOTE) {
+                                isInsideComplexCell = false;
+                                writer.print("</td>");
+                                ++index;
+                            } else {
+                                writer.print(handleSymbol(symbol));
+                                ++index;
+                            }
+                        } else {
+                            if (symbol == QUOTE) {
+                                if (line.charAt(index + 1) == COMMA) {
                                     isInsideComplexCell = false;
-                                    isComplexCellClosed = true;
-                                    ++index;
-                                } else if (line.charAt(index + 1) == COMMA) {
                                     writer.print("</td>");
-                                    isInsideComplexCell = false;
+                                    if (index == line.length() - 2) {
+                                        writer.print("<td></td>");
+                                    }
                                     index += 2;
                                 } else if (line.charAt(index + 1) == QUOTE) {
-                                    writer.print(c);
+                                    writer.print(handleSymbol(symbol));
                                     index += 2;
                                 } else {
                                     System.out.println("Неверный формат данных.");
                                     return;
                                 }
-                            } else if (isInsideSimpleCell) {
-                                System.out.println("Неверный формат данных.");
-                                return;
                             } else {
-                                isInsideComplexCell = true;
-                                writer.print("<td>");
+                                writer.print(handleSymbol(symbol));
                                 ++index;
                             }
-                            break;
-                        case COMMA:
-                            if (isInsideComplexCell) {
-                                writer.print(c);
-                                ++index;
-                            } else if (isInsideSimpleCell) {
+                        }
+                    } else {
+                        if (index == 0) {
+                            writer.print("<tr><td>");
+                            if (symbol == COMMA) {
                                 writer.print("</td>");
-                                isInsideSimpleCell = false;
+                            } else if (symbol == QUOTE) {
+                                isInsideComplexCell = true;
+                            } else {
+                                writer.print(handleSymbol(symbol));
+                            }
+                            ++index;
+                        } else if (index == line.length() - 1) {
+                            if (symbol == COMMA) {
+                                if (line.charAt(index - 1) == COMMA) {
+                                    writer.print("<td>");
+                                }
+                                writer.print("</td><td></td>");
                                 ++index;
                             } else {
-                                writer.print("<td>" + "</td>");
+                                writer.print(handleSymbol(symbol));
+                                writer.print("</td>");
                                 ++index;
                             }
-                            break;
-                        case SPACE:
-                        case TAB:
-                            if (!isInsideComplexCell && !isInsideSimpleCell) {
+                        } else {
+                            if (symbol == QUOTE) {
+                                writer.print("<td>");
+                                isInsideComplexCell = true;
+                                ++index;
+                            } else if (symbol == COMMA) {
+                                if (line.charAt(index - 1) == COMMA) {
+                                    writer.print("<td>");
+                                }
+                                writer.print("</td>");
                                 ++index;
                             } else {
-                                writer.print(c);
+                                if (line.charAt(index - 1) == COMMA && line.charAt(index + 1) != QUOTE) {
+                                    writer.print("<td>");
+                                }
+                                writer.print(handleSymbol(symbol));
                                 ++index;
                             }
-                            break;
-                        case GT:
-                            writer.print("&gt");
-                            ++index;
-                            break;
-                        case LT:
-                            writer.print("&lt");
-                            ++index;
-                            break;
-                        case AMP:
-                            writer.print("&amp");
-                            ++index;
-                            break;
-                        default:
-                            if (!isInsideComplexCell && !isInsideSimpleCell) {
-                                isInsideSimpleCell = true;
-                                writer.print("<td>" + c);
-                                ++index;
-                            } else {
-                                writer.print(c);
-                                ++index;
-                            }
+                        }
                     }
                 }
                 if (isInsideComplexCell) {
                     writer.print("<br/>");
-                } else if (isInsideSimpleCell) {
-                    isInsideSimpleCell = false;
-                    writer.println("</td>" + "</tr>");
-                } else if (isComplexCellClosed) {
-                    isComplexCellClosed = false;
-                    writer.println("</tr>");
                 } else {
-                    writer.println("<td>" + "</td>" + "</tr>");
+                    writer.println("</tr>");
                 }
             }
+
             writer.println("</table>");
             writer.println("</body>");
             writer.println("</html>");
