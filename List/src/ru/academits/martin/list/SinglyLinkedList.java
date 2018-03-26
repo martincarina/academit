a39ac74d67d7647dc07caccb41a118229d319074
martincarina <martincarina@mail.ru>
2018-03-26 15:55:53 +0700

    Создан новый подпроект List

---
+++ List/src/ru/academits/martin/list/SinglyLinkedList.java
@@ -0,0 +1,8 @@
+package ru.academits.martin.list;
+
+public class SinglyLinkedList<T> {
+    private ListItem<T> head;
+
+    public SinglyLinkedList() {
+    }
+}
---
+++ List/src/ru/academits/martin/list/exec/Main.java
@@ -0,0 +1,7 @@
+package ru.academits.martin.list.exec;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
