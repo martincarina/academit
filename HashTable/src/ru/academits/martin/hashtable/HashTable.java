617f80b671316b07d5263c3e99997ba95e6c5c9d
martincarina <martincarina@mail.ru>
2018-06-16 11:41:33 +0700

    реализован метод all  и print

--- HashTable/src/ru/academits/martin/hashtable/HashTable.java
+++ HashTable/src/ru/academits/martin/hashtable/HashTable.java
@@ -1,26 +1,52 @@
 package ru.academits.martin.hashtable;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.LinkedList;
 
 public class HashTable<T> implements Collection<T> {
-    private Item<T>[] table;
-    private int count;//количество элементов в таблице
+    //   private Item<T>[] table;
+    //   LinkedList<T>[] items;
+    private ArrayList<LinkedList<T>> items;//пока так, может, потом обычный массив попробую сделать
+    private int count;//количество в таблице
     private int size;//размер таблицы
 
-    public HashTable(int size){
+    public HashTable(int size) {
         this.size = size;
-        table = new Item[size];
+//        table = new Item[size];
+        //   this.items = new LinkedList<T>[size];
+        //   items = (LinkedList<T>[]) new Object[size];
+
+        // items = new ArrayList<LinkedList<T>>();
+        items = new ArrayList<>();
+        items.ensureCapacity(size);
+        for (int i = 0; i < size; i++) {
+            items.add(null);
+        }
     }
 
     @Override
     public int size() {
-        return size;
+        return count;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return count == 0;
+    }
+
+    public void print() {
+     /*   for (int i = 0; i < size; i++) {
+            if (items[i] != null) {
+                System.out.println(i + "" + items[i].getFirst());
+            }
+        }*/
+        for (int i = 0; i < size; i++) {
+            if (items.get(i) != null) {
+                System.out.println(i + " " + items.get(i).getFirst());
+            }
+        }
     }
 
     @Override
@@ -40,7 +66,21 @@ public class HashTable<T> implements Collection<T> {
 
     @Override
     public boolean add(Object o) {
-        return false;
+        int index = Math.abs(o.hashCode() % size);
+    /*    if (items[index] == null) {
+            //      items[index] = new LinkedList<>();
+            items[index].add((T) o);
+        }*/
+        if (items.get(index) != null) {
+            items.get(index).add((T) o);
+            ++count;
+            return true;
+        }
+        LinkedList<T> node = new LinkedList<>();
+        node.add((T) o);
+        items.set(index, node);
+        ++count;
+        return true;
     }
 
     @Override
--- HashTable/src/ru/academits/martin/hashtable/exec/Main.java
+++ HashTable/src/ru/academits/martin/hashtable/exec/Main.java
@@ -1,10 +1,14 @@
 package ru.academits.martin.hashtable.exec;
 
-import java.util.Hashtable;
+import ru.academits.martin.hashtable.HashTable;
 
 public class Main {
     public static void main(String[] args) {
-        
+        HashTable<String> hashTable = new HashTable<>(31);
+        System.out.println(hashTable.size());
+        hashTable.add("dog");
+        hashTable.add("cat");
+        hashTable.print();
+        System.out.println(hashTable.size());
     }
-
 }
