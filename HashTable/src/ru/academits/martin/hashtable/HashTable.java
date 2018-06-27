a6a91f0532224a0f201585b4a54edb6c948b21fd
martincarina <martincarina@mail.ru>
2018-06-28 01:08:57 +0700

    Удален метод print(). В итератор добавлено поле nextElement, изменен метод next(), добавлен remove(). Изменен remove(Object o). Добавлен вспомогательный метод findCollectionElements, реализованы retainAll, removeAll. Реализован toArray(T1[] a)

--- HashTable/src/ru/academits/martin/hashtable/HashTable.java
+++ HashTable/src/ru/academits/martin/hashtable/HashTable.java
@@ -3,9 +3,6 @@ package ru.academits.martin.hashtable;
 import java.util.*;
 
 public class HashTable<T> implements Collection<T> {
-    //   private Item<T>[] table;
-    //   LinkedList<T>[] items;
-    //  private ArrayList<LinkedList<T>> items;//пока так, может, потом обычный массив попробую сделать
     private static class ListItem<T> {
         private T data;
         private ListItem<T> next;
@@ -15,25 +12,19 @@ public class HashTable<T> implements Collection<T> {
             this.data = data;
         }
 
-        ListItem(T data, ListItem<T> next) {
-            this.data = data;
-            this.next = next;
-
-        }
-
         public T getData() {
             return data;
         }
 
-        public void setData(T data) {
+ /*       public void setData(T data) {
             this.data = data;
-        }
+        }*/
 
         public ListItem<T> getNext() {
             return next;
         }
 
-        public void setNext(ListItem<T> next) {
+ /*       public void setNext(ListItem<T> next) {
             this.next = next;
         }
 
@@ -43,20 +34,15 @@ public class HashTable<T> implements Collection<T> {
 
         public void setPrev(ListItem<T> next) {
             this.next = next;
-        }
+        }*/
     }
 
-    private ArrayList<ListItem<T>> items;
+    private ArrayList<ListItem<T>> items;//пока так, может, потом обычный массив попробую сделать
     private int count;//количество элементов в таблице. Если count>0.75 size. Обычно таблицу расширяют. TODO добавить проверку и расширение
-    private int size;//размер таблицы
+    private int size;
 
     public HashTable(int size) {
         this.size = size;
-//        table = new Item[size];
-        //   this.items = new LinkedList<T>[size];
-        //   items = (LinkedList<T>[]) new Object[size];
-
-        // items = new ArrayList<LinkedList<T>>();
         items = new ArrayList<>();
         items.ensureCapacity(size);
         for (int i = 0; i < size; i++) {
@@ -74,19 +60,6 @@ public class HashTable<T> implements Collection<T> {
         return count == 0;
     }
 
-    public void print() {//TODO переделать, а лучше вообще убрать, т.к. методы не должны печатать
-     /*   for (int i = 0; i < size; i++) {
-            if (items[i] != null) {
-                System.out.println(i + "" + items[i].getFirst());
-            }
-        }*/
-        for (int i = 0; i < size; i++) {
-            if (items.get(i) != null) {
-                System.out.println(i + " " + items.get(i).getData());
-            }
-        }
-    }
-
     @Override
     public boolean contains(Object o) {
         if (o == null) {
@@ -94,25 +67,22 @@ public class HashTable<T> implements Collection<T> {
         }
         int index = Math.abs(o.hashCode() % size);
         if (items.get(index) != null) {
-/*            if (items.get(index).getData().equals(o)) {
-                return true;
-            } else {*/
-            MyIterator listIterator = new MyIterator();
+            Iterator listIterator = new MyIterator();
+            //          Iterator<T> listIterator = this.iterator();//можно foreach попробовать
             while (listIterator.hasNext()) {
                 if (listIterator.next().equals(o)) {
                     return true;
                 }
             }
-//            }
         }
         return false;
-//        return items.get(index) != null;//TODO может, надо еще equals применить, а то вдруг просто hashCode совпал
     }
 
-    private class MyIterator implements Iterator<T> {// TODO возможно, нужен modCount
-        int currentElementNumber = 0;//номер текущего элемента таблицы
-        int currentArrayIndex = 0;//индекс текущего элемента таблицы
+    private class MyIterator implements Iterator<T> {// TODO наверное, нужен modCount
+        int currentElementNumber = 0;
+        int currentArrayIndex = 0;
         ListItem<T> currentElement;
+        ListItem<T> nextElement;
 
         @Override
         public boolean hasNext() {
@@ -123,20 +93,44 @@ public class HashTable<T> implements Collection<T> {
         public T next() {
             for (; currentElementNumber != count; currentArrayIndex++) {
                 if (items.get(currentArrayIndex) != null) {
+                    currentElement = nextElement;
                     if (currentElement == null) {
                         currentElement = items.get(currentArrayIndex);
                     }
 
                     currentElementNumber++;
                     T value = currentElement.getData();
-                    currentElement = currentElement.getNext();
-                    if (currentElement == null) {
+                    nextElement = currentElement.getNext();
+                    if (nextElement == null) {
                         currentArrayIndex++;
                     }
                     return value;
                 }
             }
             return null;
+        }
+
+        @Override
+        public void remove() {
+            if (currentElementNumber < 0) {
+                throw new IllegalStateException();
+            }
+     /*       if (modCount != currentModCount) {
+                throw new ConcurrentModificationException();
+            }*/
+
+            if (currentElement.prev != null) {
+                currentElement.prev.next = currentElement.next;
+            } else {
+                items.set(currentArrayIndex, currentElement.next);
+            }
+            if (currentElement.next != null) {
+                currentElement.next.prev = currentElement.prev;
+            }
+            --count;
+            --currentElementNumber;
+            currentElement = null;
+            //           currentModCount = modCount;
         }
     }
 
@@ -146,15 +140,9 @@ public class HashTable<T> implements Collection<T> {
     }
 
     @Override
-    public Object[] toArray() {//TODO Должен быть массив со всеми значениями коллекции
+    public Object[] toArray() {
         T[] array = (T[]) new Object[count];
         Iterator<T> listIterator = this.iterator();
- /*
-  int i = 0;
-  while (listIterator.hasNext()) {
-            array[i] = listIterator.next();
-            i++;
-        }*/
 
         for (int j = 0; j < array.length; j++) {
             //           if (listIterator.hasNext()) { - надо ли? массив ведь длиной с количество элементов таблицы
@@ -166,8 +154,8 @@ public class HashTable<T> implements Collection<T> {
 
     @Override
     public boolean add(Object o) {
-        if(o==null){
-            throw new NullPointerException("В аргумент передан null");//от null hashcode не считается.
+        if (o == null) {
+            throw new NullPointerException("В аргумент передан null");
         }
         ListItem<T> node = new ListItem<>((T) o);
         int index = Math.abs(o.hashCode() % size);
@@ -183,7 +171,7 @@ public class HashTable<T> implements Collection<T> {
     @Override
     public boolean remove(Object o) {
         if (o == null) {
-            throw new NullPointerException("В аргумент передан null");
+            return false;
         }
         int index = Math.abs(o.hashCode() % size);
         if (items.get(index) == null) {
@@ -200,12 +188,12 @@ public class HashTable<T> implements Collection<T> {
                 if (node.next != null) {
                     node.next.prev = node.prev;
                 }
-                break;
+                --count;
+                return true;
             }
             node = node.next;
         }
-        --count;
-        return true;
+        return false;
     }
 
     @Override
@@ -232,14 +220,19 @@ public class HashTable<T> implements Collection<T> {
         count = 0;
     }
 
+    private boolean findCollectionElements(Collection<?> c, boolean type) {
+        Objects.requireNonNull(c);
+        return this.removeIf((T value) -> c.contains(value) != type);
+    }
+
     @Override
     public boolean retainAll(Collection c) {
-        return false;
+        return findCollectionElements(c, true);
     }
 
     @Override
     public boolean removeAll(Collection c) {
-        return false;
+        return findCollectionElements(c, false);
     }
 
     @Override
@@ -256,10 +249,21 @@ public class HashTable<T> implements Collection<T> {
     }
 
     @Override
-    public Object[] toArray(Object[] a) {
+    public <T1> T1[] toArray(T1[] a) {
         if (a == null) {
             throw new NullPointerException("Передан null");
         }
-        return new Object[0];
+        if (a.length < count) {
+            Object[] array = this.toArray();
+            return (T1[]) Arrays.copyOf(array, count, a.getClass());
+        }
+        Iterator<T> listIterator = this.iterator();
+        for (int j = 0; j < count; j++) {
+            a[j] = (T1) listIterator.next();
+        }
+        if (a.length > count) {
+            a[count] = null;
+        }
+        return a;
     }
 }
--- HashTable/src/ru/academits/martin/hashtable/exec/Main.java
+++ HashTable/src/ru/academits/martin/hashtable/exec/Main.java
@@ -7,25 +7,27 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        HashTable<String> hashTable = new HashTable<>(1);
+        HashTable<String> hashTable = new HashTable<>(10);
+        System.out.println("Размер таблицы");
         System.out.println(hashTable.size());
+        System.out.println("Добавление элементов");
         hashTable.add("dog");
         hashTable.add("cat");
+        System.out.println("Элементы таблицы");
         for (String s : hashTable) {
             System.out.println(s);
         }
+        System.out.println("Размер таблицы");
         System.out.println(hashTable.size());
+        System.out.println("Поиск элемента в таблице");
         System.out.println(hashTable.contains("dog"));
-//        hashTable.clear(); - работает нормально. Тест в конец перенести
-        System.out.println(hashTable.size());
-        System.out.println("Перевод коллекции в массив");
+        System.out.println("Перевод таблицы в массив");
         Object[] array = hashTable.toArray();
         for (Object s : array) {
             System.out.println(s);
         }
         System.out.println("Удаление элемента из таблицы");
         System.out.println(hashTable.remove("dog"));
-//        hashTable.add(null);
         System.out.println("Таблица после удаления элемента");
         for (String s : hashTable) {
             System.out.println(s);
@@ -37,7 +39,32 @@ public class Main {
         for (String s : hashTable) {
             System.out.println(s);
         }
+        System.out.println("Поиск коллекции в таблице");
         ArrayList<String> lines0 = new ArrayList<>(Arrays.asList("cat", "dog", null));
         System.out.println(hashTable.containsAll(lines0));
+
+        System.out.println("Перевод таблицы в существующий массив");
+        String[] array1 = hashTable.toArray(new String[0]);
+        for (Object s : array1) {
+            System.out.println(s);
+        }
+
+        ArrayList<String> lines1 = new ArrayList<>(Arrays.asList("a", "b"));
+        System.out.println("Удаление коллекции");
+        System.out.println(hashTable.removeAll(lines1));
+        System.out.println("После удаления:");
+        for (String s : hashTable) {
+            System.out.println(s);
+        }
+        System.out.println("Оставить элементы коллекции");
+        hashTable.retainAll(lines);
+        System.out.println("После применения метода retainAll:");
+        for (String s : hashTable) {
+            System.out.println(s);
+        }
+        System.out.println("Очистка таблицы");
+        hashTable.clear();
+        System.out.println("Размер таблицы");
+        System.out.println(hashTable.size());
     }
 }
