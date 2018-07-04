f6959b28e9d55c973915957b7eb427140447f915
martincarina <martincarina@mail.ru>
2018-07-04 22:24:00 +0700

    Выполнена Задача 1 в Lambdas

--- Lambdas/src/ru/academits/martin/lambdas/Person.java
+++ Lambdas/src/ru/academits/martin/lambdas/Person.java
@@ -4,7 +4,7 @@ public class Person {
     private String name;
     private int age;
 
-    public Person(String name, int age){
+    public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
@@ -13,9 +13,15 @@ public class Person {
         return name;
     }
 
-    public int getAge(){
-    return age;
+    public int getAge() {
+        return age;
     }
-    
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
